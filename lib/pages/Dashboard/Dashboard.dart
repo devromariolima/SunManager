6e03b994fbab0264dbe921e056fbf29e40e25fff
Romário Lima <devromariolima@gmail.com>
2025-03-03 17:45:49 -0400

    Adiciona página de Integradores e implementa loading na navegação do Dashboard

--- lib/pages/Dashboard/Dashboard.dart
+++ lib/pages/Dashboard/Dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:cripto/pages/Graphics/carteira_page.dart';
+import 'package:cripto/pages/Integrators/Integrators_page.dart';
 import 'package:cripto/pages/OrdersPage/Orders_page.dart';
 import 'package:cripto/pages/Settings/Configuracoes_page.dart';
 import 'package:flutter/cupertino.dart';
@@ -112,7 +113,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     () => Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const ConfiguracoesPage())),
+                            builder: (context) => const IntegratorsPage())),
                   ),
                   itemDashboard(
                     'Meu Perfil',
@@ -168,46 +169,63 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  itemDashboard(String title, IconData iconData, Color background,
-          VoidCallback onTap) =>
-      InkWell(
-        onTap: onTap, // Detecta o toque
-        borderRadius: BorderRadius.circular(10), // Adiciona feedback visual
-        splashColor: Colors.blue.withOpacity(0.3), // Efeito de splash ao tocar
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                offset: const Offset(0, 5),
-                color: Theme.of(context).primaryColor.withOpacity(.2),
-                spreadRadius: 2,
-                blurRadius: 5,
-              ),
-            ],
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                decoration:
-                    BoxDecoration(color: background, shape: BoxShape.circle),
-                child: Icon(
-                  iconData,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-                title,
-                style: Theme.of(context).textTheme.titleMedium,
-              )
-            ],
-          ),
+  itemDashboard(
+      String title, IconData iconData, Color background, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: () async {
+        // Exibe o loading antes de executar a ação
+        showDialog(
+          context: context,
+          barrierDismissible:
+              false, // Impede que o loading seja fechado ao clicar fora
+          builder: (context) =>
+              const Center(child: CircularProgressIndicator()),
+        );
+
+        // Simula um tempo de carregamento
+        await Future.delayed(const Duration(seconds: 2));
+
+        // Fecha o loading
+        Navigator.pop(context);
+
+        // Chama a função onTap após o tempo de carregamento
+        onTap();
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 5),
+              color: Theme.of(context).primaryColor.withOpacity(.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+            ),
+          ],
         ),
-      );
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              decoration:
+                  BoxDecoration(color: background, shape: BoxShape.circle),
+              child: Icon(
+                iconData,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
